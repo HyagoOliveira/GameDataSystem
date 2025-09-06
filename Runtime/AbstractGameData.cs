3a3901abfcd27cd5f2dd30c164c25f506fc88b9f
Hyago Oliveira <hyagogow@gmail.com>
2025-09-06 15:41:12 -0300

    Add code documentation

--- Runtime/AbstractGameData.cs
+++ Runtime/AbstractGameData.cs
@@ -8,13 +8,30 @@ namespace ActionCode.GameDataSystem
     /// </summary>
     public abstract class AbstractGameData : ScriptableObject
     {
-        [SerializeField] private int slotIndex;
+        [SerializeField, Tooltip("The Game Slot index for this Data.")]
+        private int slotIndex;
 
+        /// <summary>
+        /// The Game Slot index for this Data.
+        /// </summary>
         public int SlotIndex => slotIndex;
+
+        /// <summary>
+        /// The language code following the Localization System convention.
+        /// </summary>
         public string LanguageCode { get; set; }
+
+        /// <summary>
+        /// The time this Data was created.
+        /// </summary>
         public DateTime Created { get; private set; } = DateTime.Now;
+
+        /// <summary>
+        /// The last time this Data was updated.
+        /// </summary>
         public DateTime LastUpdate { get; private set; } = DateTime.Now;
 
+        public bool HasInvalidLanguage() => string.IsNullOrEmpty(LanguageCode);
 
         public void UpdateData(int slot)
         {
@@ -30,9 +47,7 @@ namespace ActionCode.GameDataSystem
             JsonUtility.FromJsonOverwrite(json, this);
         }
 
-        public virtual string GetDisplayName() => $"Slot {SlotIndex}";
+        public virtual string GetDisplayName() => $"Game Data {SlotIndex:D2}";
         public override string ToString() => GetDisplayName();
-
-        public bool HasInvalidLanguage() => string.IsNullOrEmpty(LanguageCode);
     }
 }
