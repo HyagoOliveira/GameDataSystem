349ed0a252436bbe995a615d659d45330a2d937f
Hyago Oliveira <hyagogow@gmail.com>
2025-10-15 14:24:08 -0300

    Add CloudProviderType

---
+++ Runtime/CloudSave/CloudProviderType.cs
@@ -0,0 +1,11 @@
+namespace ActionCode.GameDataSystem
+{
+    /// <summary>
+    /// All available Cloud Provider types.
+    /// </summary>
+    public enum CloudProviderType
+    {
+        None,
+        UnityCloud
+    }
+}
